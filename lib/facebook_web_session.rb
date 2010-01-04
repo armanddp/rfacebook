0408eac16983a8104096155389f97e3234bfafdd
Armand du Plessis <armanddp@agilisto.com>
2010-01-05 01:15:18 +0200

    Returning session information by default when login required

--- lib/facebook_web_session.rb
+++ lib/facebook_web_session.rb
@@ -35,10 +35,11 @@ module RFacebook
         
     # Gets the authentication URL for this application
     #
-    # options.next::          the page to redirect to after login
-    # options.popup::         boolean, whether or not to use the popup style (defaults to false)
-    # options.skipcookie::    boolean, whether to force new Facebook login (defaults to false)
-    # options.hidecheckbox::  boolean, whether to show the "infinite session" option checkbox
+    # options.next::           the page to redirect to after login
+    # options.popup::          boolean, whether or not to use the popup style (defaults to false)
+    # options.skipcookie::     boolean, whether to force new Facebook login (defaults to false)
+    # options.hidecheckbox::   boolean, whether to show the "infinite session" option checkbox
+    # options.return_session:: boolean, whether to return the session information in the initial call. (defaults to true)
     def get_login_url(options={})
       # handle options
       nextPage = options[:next] ||= nil
@@ -47,7 +48,7 @@ module RFacebook
       hidecheckbox = (options[:hidecheckbox] == nil) ? false : true
       frame = (options[:frame] == nil) ? false : true
       canvas = (options[:canvas] == nil) ? false : true
-      return_session = (options[:return_session] == nil) ? false : true
+      return_session = (options[:return_session] == nil) ? true : false
     
       # url pieces
       optionalNext = (nextPage == nil) ? "" : "&next=#{CGI.escape(nextPage.to_s)}"
